3de88a74aa723c76ecc2aef19a5b5eeb8028c9ea
likivik <likivik@gmail.com>
2026-04-20 01:47:25 +0300

    test

---
+++ modules/def-determinate.nix
@@ -0,0 +1,15 @@
+{ inputs, ... }: {
+
+  flake-file.inputs = {
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
+  };
+
+  imports = [ inputs.determinate.nixosModules.default ];
+
+  # den.aspects.determinate = {
+  #   nixos = { ... }: {
+  #     imports = [ inputs.determinate.nixosModules.default ];
+  #   };
+  # };
+
+}

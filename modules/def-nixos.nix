900d1a90ed11edf5ef542335491383dfd5d26cff
likivik <likivik@gmail.com>
2026-04-20 01:45:29 +0300

    test

--- modules/def-nixos.nix
+++ modules/def-nixos.nix
@@ -3,9 +3,9 @@
 {
   # Main nixpkgs source
   flake-file.inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # Determinate unstable
+    # nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # flakehub unstable
     # nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   den.default.nixos.system.stateVersion = "25.11";

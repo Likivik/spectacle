90b947715bb16842aedb93fb41b4960f4dccb3b3
likivik <likivik@gmail.com>
2026-04-20 03:48:52 +0300

    wtf

---
+++ modules/inputs.nix
@@ -0,0 +1,21 @@
+# This file: pulls stuff needed for den/dendritic stuff
+{ inputs, ... }:
+{
+  imports = [
+    (inputs.flake-file.flakeModules.dendritic or { })
+    (inputs.den.flakeModules.dendritic or { })
+  ];
+
+  # other inputs may be defined at a module using them.
+  flake-file.inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # flakehub unstable
+    # nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    den.url = "github:vic/den";
+    flake-file.url = "github:vic/flake-file";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+}

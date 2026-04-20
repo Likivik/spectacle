1c7b69afcdd05d9d687c12b0405887c17fb7152b
likivik <likivik@gmail.com>
2026-04-20 05:46:33 +0300

    wow

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -5,13 +5,15 @@
   den.default.nixos.system.stateVersion = "25.11";
   den.default.homeManager.home.stateVersion = "25.11";
 
-    # These are functions that produce configs
+  # These are functions that produce configs
   den.default.includes = [ 
     den.provides.define-user # Automatically create users + their homes, by just adding them to hosts
     # TODO: ??? does this not happen automatically? ${user}.provides.${host} and ${host}.provides.${user}
     den.provides.mutual-provider
     # TODO: ??? this Automatically sets hostname, but isn't it already automatically set as per host schema?
     den.provides.hostname 
-
+    # determinate systems `nix`
+    den.aspects.determinateNix
   ];
+
 }

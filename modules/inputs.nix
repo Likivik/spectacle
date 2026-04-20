8ff59ed68dc3121b06a61fe820b07a08a5311f9d
likivik <likivik@gmail.com>
2026-04-20 04:52:50 +0300

    sss

--- modules/inputs.nix
+++ modules/inputs.nix
@@ -4,14 +4,14 @@
   imports = [
     (inputs.flake-file.flakeModules.dendritic or { })
     (inputs.den.flakeModules.dendritic or { })
-    inputs.determinate.nixosModules.default
+    # inputs.determinate.nixosModules.default
   ];
 
   # other inputs may be defined at a module using them.
   flake-file.inputs = {
-    # nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # flakehub unstable
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # flakehub unstable
     # nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     den.url = "github:vic/den";
     flake-file.url = "github:vic/flake-file";
     home-manager = {

176bb30c85d7fb7462e9716884da638666cc55be
likivik <likivik@gmail.com>
2026-04-20 03:14:42 +0300

    test

--- modules/def-den.nix
+++ modules/def-den.nix
@@ -4,12 +4,14 @@
   imports = [
     (inputs.flake-file.flakeModules.dendritic or { })
     (inputs.den.flakeModules.dendritic or { })
+    inputs.determinate.nixosModules.default
   ];
 
   # other inputs may be defined at a module using them.
   flake-file.inputs = {
     den.url = "github:vic/den";
     flake-file.url = "github:vic/flake-file";
+    determinate.url = "github:DeterminateSystems/determinate";
   };
 
   # These are functions that produce configs

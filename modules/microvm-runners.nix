a2f100fce62f6b1204f9302ccd2ebabf64801543
likivik <likivik@gmail.com>
2026-05-03 21:16:00 +0300

    +microvm

---
+++ modules/microvm-runners.nix
@@ -0,0 +1,40 @@
+# for each host exposes microvm declaredRunner (if exists) as package output of this flake.
+# feel free to remove or adapt.
+{
+  den,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+let
+  microvmRunners = lib.pipe den.hosts [
+    lib.attrValues
+    (lib.concatMap lib.attrValues)
+    (map (
+      host:
+      let
+        osConf = lib.attrByPath host.intoAttr null config.flake;
+        vmRunner = osConf.config.microvm.declaredRunner or null;
+        package = lib.optionalAttrs (vmRunner != null) {
+          ${host.system}.${host.name} = vmRunner;
+        };
+      in
+      package
+    ))
+  ];
+in
+{
+
+  nixConfig = {
+    extra-substituters = [ "https://microvm.cachix.org" ];
+    extra-trusted-public-keys = [ "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys=" ];
+  };
+  inputs.microvm = {
+    url = "github:microvm-nix/microvm.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+
+  config.flake.packages = lib.mkMerge microvmRunners;
+}

955b16e93fdf5bd78589499f0aef95773d83aa68
likivik <likivik@gmail.com>
2026-05-03 21:19:22 +0300

    +gnome extensions

--- modules/aspects/desktops/gnome.nix
+++ modules/aspects/desktops/gnome.nix
@@ -4,6 +4,49 @@
 
     nixos =
       { pkgs, lib, ... }:
+      let
+        enabledExtensions = with pkgs.gnomeExtensions; [
+          appindicator
+          clipboard-history
+          gsconnect
+          # impatience
+          caffeine
+          open-bar
+          blur-my-shell
+          status-area-horizontal-spacing
+          tailscale-status
+          tiling-assistant
+          transcodeappsearch
+          vertical-workspaces
+          window-gestures
+          just-perfection
+          arc-menu
+          gjs-osk # slightly better on-screen-keyboard
+          places-status-indicator
+          app-grid-button-in-top-panel
+          extension-list
+
+        ];
+        availableExtensions = with pkgs.gnomeExtensions; [
+          touchup
+          screenshot-ocr
+          auto-activities
+          clipboard-indicator
+          user-themes
+          status-area-horizontal-spacing
+          vitals
+          bottom-dash-panel
+          dash-to-panel
+          dash-to-dock
+          keep-pinned-apps-in-appgrid
+          quick-settings-tweaker
+          systemd-manager
+          fly-pie
+          kando-integration
+          simpleweather
+
+        ];
+      in
       {
 
         services.displayManager.gdm.enable = true;
@@ -31,69 +74,62 @@
             refine # maybe better than tweaks
             ocs-url
             ibus-engines.typing-booster # autocompletion!!! https://github.com/mike-fabian/ibus-typing-booster
+            tesseract
 
           ])
-          ++ (with pkgs.gnomeExtensions; [
-            appindicator
-            clipboard-history
-            gsconnect
-            # impatience
-            keep-awake
-            open-bar
-            blur-my-shell
-            status-area-horizontal-spacing
-            tailscale-status
-            tiling-assistant
-            transcodeappsearch
-            vertical-workspaces
-            window-gestures
-            # fly-pie
-            kando-integration
-            just-perfection
-            arc-menu
-            gjs-osk # better on-screen-keyboard
-          ]);
+          ++ enabledExtensions
+          ++ availableExtensions;
 
         programs.dconf = {
           enable = true;
-          profiles.user.databases = {
-            # Mutter Settings
-            "org/gnome/mutter" = {
-              experimental-features = [
-                "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
-                "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
-                "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
-                "autoclose-xwayland" # automatically terminates Xwayland if all relevant X11 clients are gone
-              ];
-            };
+          profiles.user.databases = [
+            {
+              settings = {
+                # Mutter Settings
+                "org/gnome/mutter" = {
+                  experimental-features = [
+                    "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
+                    "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
+                    "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
+                    "autoclose-xwayland" # automatically terminates Xwayland if all relevant X11 clients are gone
+                  ];
+                };
 
-            "org/gnome/desktop/input-sources" = {
-              sources = [
-                (lib.hm.gvariant.mkTuple [
-                  "xkb"
-                  "us"
-                ])
-                (lib.hm.gvariant.mkTuple [
-                  "xkb"
-                  "ru"
-                ])
-              ];
-            };
+                "org/gnome/desktop/input-sources" = {
+                  sources = [
+                    (lib.gvariant.mkTuple [
+                      "xkb"
+                      "us"
+                    ])
+                    (lib.gvariant.mkTuple [
+                      "xkb"
+                      "ru"
+                    ])
+                  ];
+                };
 
-            # Extensions Part
-            "org/gnome/shell" = {
-              disable-user-extensions = false; # enables user extensions
-              disabled-extensions = [ ];
-            };
+                /*
+                  --------------------------------------------------------------
+                                  Window Manager: Normal Buttons
+                  ---------------------------------------------------------------
+                */
+                "org/gnome/desktop/wm/preferences" = {
+                  button-layout = "appmenu:minimize,maximize,close";
+                };
 
-            #"org/gnome/shell".enabled-extensions = map (extension: extension.extensionUuid) home.packages;
+                # Extensions Part
+                "org/gnome/shell" = {
+                  disable-user-extensions = false; # enables user extensions
+                  enabled-extensions = map (extension: extension.extensionUuid) enabledExtensions;
+                };
 
-            # Configure individual extensions
+                # Configure individual extensions
 
-            # Configure GSConnect
-            "org/gnome/shell/extensions/gsconnect".show-indicators = true;
-
-          };
+                # Configure GSConnect
+                "org/gnome/shell/extensions/gsconnect".show-indicators = true;
+              };
+            }
+          ];
         };
 
       };

e6ee9eae5276a5b55730a3d283f3baf9f3e1349d
likivik <likivik@gmail.com>
2026-05-19 16:23:39 +0300

    host and user definition updates

--- modules/hosts/host-user-definitions.nix
+++ modules/hosts/host-user-definitions.nix
@@ -2,27 +2,58 @@
 { inputs, ... }:
 
 {
-  den.hosts.x86_64-linux.serenity = {
-    description = "Desktop";
-    users = {
-      likivik = { };
-    };
-  };
 
-  # den.hosts.x86_64-linux.traversal = {
-  #   description = "Laptop";
-  #   users = {
-  #     likivik = { };
-  #   };
-  # };
+  den.hosts.x86_64-linux = {
 
-  den.hosts.x86_64-linux.spectacle = {
-    description = "Small box connected to TV to watch shows, movies and use browser";
-    users = {
-      watcher = { };
+    serenity = {
+      description = "Likivik's Desktop";
+      users = {
+        likivik = { };
+      };
     };
 
-  };
+    traversal = {
+      description = "Likivik's Laptop";
+      users = {
+        likivik = { };
+      };
+    };
 
+    spectacle = {
+      description = "Small box connected to TV to watch shows, movies and use browser";
+      users = {
+        watcher = { };
+      };
+    };
+
+    salembox = {
+      description = "Salem's Laptop";
+      users = {
+        salem = { };
+      };
+    };
+
+    homelab01-poweredge = {
+      description = "homelab01";
+      users = {
+        likivik = { };
+      };
+    };
+
+    devbox01 = {
+      description = "Devbox01";
+      users = {
+        likivik = { };
+      };
+    };
+
+    nixosrouter = {
+      description = "Router";
+      users = {
+        likivik = { };
+      };
+    };
+
+  };
 
 }

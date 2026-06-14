2935189862bd1246735c4e68f1020f923c4a1efe
likivik <likivik@gmail.com>
2026-06-14 10:36:27 +0300

    refactor(traversal): inline cursor derivations, add AMD GPU config

--- modules/hosts/traversal/traversal.nix
+++ modules/hosts/traversal/traversal.nix
@@ -1,4 +1,10 @@
-{ den, lib, inputs, ... }: {
+{
+  den,
+  lib,
+  inputs,
+  ...
+}:
+{
   den.aspects.traversal = {
 
     includes = [
@@ -8,50 +14,72 @@
       den.aspects.desktop.desktopManagers.dank-material-shell
 
       den.aspects.dev
-      
+
       den.aspects.firefox
     ];
 
-    nixos = {config, modulesPath, ... }: {
-      imports = [ (modulesPath + "/installer/scan/not-detected.nix")];
+    nixos =
+      { config, modulesPath, ... }:
+      {
+        imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-      # services.openssh.enable = true;
+        # services.openssh.enable = true;
 
-      boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" "sdhci_pci" ];
-      boot.initrd.kernelModules = [ ];
-      boot.kernelModules = [ "kvm-amd" ];
-      boot.extraModulePackages = [ ];
+        hardware.amdgpu.initrd.enable = lib.mkDefault true;
 
-      fileSystems."/" =
-        { device = "/dev/disk/by-uuid/ef9e9419-e98c-4667-a569-e1afeedf3df4";
+        services.xserver.videoDrivers = lib.mkDefault [ "modesetting" ];
+
+        hardware.graphics = {
+          enable = lib.mkDefault true;
+          enable32Bit = lib.mkDefault true;
+        };
+
+        services.tlp.enable = lib.mkDefault (!config.services.power-profiles-daemon.enable);
+
+        services.fstrim.enable = lib.mkDefault true;
+
+        boot.initrd.availableKernelModules = [
+          "nvme"
+          "xhci_pci"
+          "thunderbolt"
+          "usb_storage"
+          "sd_mod"
+          "sdhci_pci"
+        ];
+        boot.initrd.kernelModules = [ ];
+        boot.kernelModules = [ "kvm-amd" ];
+        boot.extraModulePackages = [ ];
+
+        fileSystems."/" = {
+          device = "/dev/disk/by-uuid/ef9e9419-e98c-4667-a569-e1afeedf3df4";
           fsType = "btrfs";
         };
 
-      fileSystems."/boot" =
-        { device = "/dev/disk/by-uuid/CABA-4BEE";
+        fileSystems."/boot" = {
+          device = "/dev/disk/by-uuid/CABA-4BEE";
           fsType = "vfat";
-          options = [ "fmask=0077" "dmask=0077" ];
+          options = [
+            "fmask=0077"
+            "dmask=0077"
+          ];
         };
 
-      swapDevices =
-        [ { device = "/dev/disk/by-uuid/1385a7cc-ed8e-4567-a77d-0a11bf7f8ac1"; }
+        swapDevices = [
+          { device = "/dev/disk/by-uuid/1385a7cc-ed8e-4567-a77d-0a11bf7f8ac1"; }
         ];
 
-      # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-      # (the default) this is the recommended approach. When using systemd-networkd it's
-      # still possible to use this option, but it's recommended to use it in conjunction
-      # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-      networking.useDHCP = lib.mkDefault true;
-      # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
-      # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+        # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+        # (the default) this is the recommended approach. When using systemd-networkd it's
+        # still possible to use this option, but it's recommended to use it in conjunction
+        # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+        networking.useDHCP = lib.mkDefault true;
+        # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
+        # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
-      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-      hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+        nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+        hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-      systemd.user.services.dms.environment.RADV_PERFTEST = lib.mkIf config.programs.dms-shell.enable "cswt";
-
-    };
-
+      };
 
   };
 }

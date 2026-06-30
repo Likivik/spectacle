84e2cdca703ff2c0fe4651c5f886029f5c4f2c2b
likivik <likivik@gmail.com>
2026-06-30 14:56:39 +0300

    bump: regenerate flake.nix + flake.lock (add hermes-agent input)

--- flake.nix
+++ flake.nix
@@ -16,6 +16,10 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    hermes-agent = {
+      url = "github:NousResearch/hermes-agent";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";

436cbce7ccc5a78343c218ba45b9b31d59f160a2
likivik <likivik@gmail.com>
2026-07-20 02:31:47 +0300

    feat(ssh): add hermes@erebus SSH key to serenity and poweredge

--- modules/hosts/poweredge/poweredge.nix
+++ modules/hosts/poweredge/poweredge.nix
@@ -28,8 +28,10 @@
         extraGroups = [ "wheel" ];
         hashedPassword = "$y$j9T$CflOiBqf7adw8VPM1HjjD0$I.UH24kDyF8m75kAe3c4pO87oujxnUah7vBIDuTetR9";
         openssh.authorizedKeys.keys = [
-          # erebus
+          # erebus (VPS provider default)
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEhOOKKg6lHLhp2x3lAIg6bFheG8SlN+vsnFeTtIRBLo root@bistre-prase24161"
+          # hermes@erebus
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIECMxs9cBFN8Adq8AJ9I62gVNFTkgNkr0ikg+VkWbHx1 hermes@erebus"
           # traversal
           "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLeI2EqFsNLBPNIi/neXss0yZ3Q0vLevkiK5gfF5Fc+Zo0i9Nf0JPPkq3ak+uc5wJvumSvMAgO+gUUxDbQ6ieMZKCU6HSEhcQvjiHKczyYx+mDxxz6TXnd9TQRUFwmM/u/5kocl9PIwzjDnEdC/84H4sKiv9tmCy6Lv97VpdTYwkYerNWPm3wiapfGROHcS1WjKFOTD7+S++SQLDzir07W509b15HzgiP0Mk7Jdcc3axfIVl/FykGUQeYEFCram0XHvlDIB4yCb9rFxVACQXvUFgXLLb942lvoKeg5d2HbOxLXRVFlJJCnJlYQB3aKis983zjNmZ18Pm21YYvG6vmH traversal-likivik-2024-07-rsa"
           # likivik@gmail.com
@@ -70,6 +72,10 @@
         device = "/swapfile";
         size = 8192; # 8GB
       }];
+
+      boot.supportedFilesystems = [ "zfs" ];
+      boot.zfs.extraPools = [ "tank" ];
+      networking.hostId = "5a099923";
 
       services.zfs.autoScrub.enable = true;
     };

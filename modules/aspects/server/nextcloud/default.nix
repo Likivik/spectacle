940932fcfbd7a1991d260f32c8cd7faee6816530
likivik <likivik@gmail.com>
2026-07-23 19:57:59 +0300

    fix(nextcloud): declare trusted proxies for Tailscale Serve reverse proxy

--- modules/aspects/server/nextcloud/default.nix
+++ modules/aspects/server/nextcloud/default.nix
@@ -45,6 +45,8 @@
 
         settings = {
           default_phone_region = "RU";
+          trusted_proxies = [ "127.0.0.1" "::1" ];
+          forwarded_for_headers = [ "HTTP_X_FORWARDED_FOR" ];
           trusted_domains = [
             "nextcloud.likivik.com"
             "poweredge"

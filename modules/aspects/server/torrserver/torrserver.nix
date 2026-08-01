436d5e29060321098864255e70d81d19be7d3f95
likivik <likivik@gmail.com>
2026-08-01 14:55:46 +0300

    fix(torrserver): dedupe user config, hardcode torrentsDir
    - move user config out of dynamic user block (was duplicating option declarations)
    - set torrentsDir to /Storage/Torrents, grant torrserver group to likivik+watcher
    
    sync(dankmaterialshell): update selected KDE Connect device id
    
    chore: add plan-korrekcii-qr.md

--- modules/aspects/server/torrserver/torrserver.nix
+++ modules/aspects/server/torrserver/torrserver.nix
@@ -7,21 +7,21 @@
   };
 
   den.aspects.torrserver = {
-    user = { user, ... }: {
-      nixos.users.users.${user.userName}.extraGroups = [ "torrserver" ];
-      nixos.services.torrserver.torrentsDir = "/home/${user.userName}/Torrents";
-    };
-
+    # TODO: make user management dynamic instead of hardcoding likivik+watcher
+    # (user block causes Den to evaluate nixos block per-user, duplicating
+    #  option declarations from the torrserver flake module import)
     nixos =
       { config, ... }:
       {
-        imports = [ inputs.torrserver.nixosModules.default ]; # import torrserver flake, so that config options become available!
-        # "https://github.com/YouROK/TorrServer/blob/master/nix/modules/nixos.nix"
+        imports = [ inputs.torrserver.nixosModules.default ];
         services.torrserver = {
           enable = true;
           maxCacheSize = "5GB";
           enableDLNA = true;
+          torrentsDir = "/Storage/Torrents";
         };
+        users.users.likivik.extraGroups = [ "torrserver" ];
+        users.users.watcher.extraGroups = [ "torrserver" ];
       };
   };
 }

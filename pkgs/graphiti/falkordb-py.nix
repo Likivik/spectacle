2a966db78a33209742067944affa278d175db2bb
Likivik <likivik@gmail.com>
2026-08-06 23:14:07 +0300

    fix(pkgs/graphiti/falkordb-py): use setuptools format

--- pkgs/graphiti/falkordb-py.nix
+++ pkgs/graphiti/falkordb-py.nix
@@ -8,12 +8,16 @@ buildPythonPackage rec {
   pname = "falkordb";
   version = "1.6.2";
 
+  format = "setuptools";
+
   src = fetchPypi {
     inherit pname version;
     sha256 = "sha256-b17571ebf4d65dbd3588e8c470b16f0ddfea820e0a15ed73af0dd1a4728b480d";
   };
 
   doCheck = false;
+
+  pythonImportsCheck = [ "falkordb" ];
 
   meta = {
     description = "Python client for FalkorDB";
